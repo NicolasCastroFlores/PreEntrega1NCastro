62632e21f3d4f1855edfd950a187b3bc8d6c6901
unknown <nicolascastro2121@gmail.com>
2023-10-01 23:42:06 -0300

    cambios en la swicth principal

--- app.js
+++ app.js
@@ -34,93 +34,93 @@ switch (pelicula) {
     default:
         console.log("Oppenheimer es la opción recomendada del día");
         break;
-
-        alert("Debemos verificar tu edad si quieres ver las películas en cartelera")
-
-        let edad = prompt("ingresa tu edad a continuacion")
-
-        if (edad >= 18) {
-            console.log("Ahora puedes continuar")
-        } else {
-            console.log("Para ver estas peliculas debes ser mayor de edad")
-        }
-        let extras = "";
-        let extrastotal = 0;
-
-        while (extras !== "0" && extras !== "5") {
-            extras = prompt(`¡Elige que llevar!
-
-    1. Gaseosa Grande (+$2500)
-    2. Pochoclos Grande (+4000)
-    3. Cerveza -lata- (+2000$)
-    4. 2x1 Pochoclos Medianos y Dos Gaseosas (+7000)
-    5. Continuar sin consumir
-    0. Ingresa 0 para finalizar.
-    `);
-
-            switch (extras) {
-                case "1":
-                    extrastotal += 2500;
-                    break;
-                case "2":
-                    extrastotal += 4000;
-                    break;
-                case "3":
-                    extrastotal += 2000;
-                    break;
-                case "4":
-                    extrastotal += 7000;
-                    break;
-                case "5":
-                    break;
-                case "0":
-                    break;
-                default:
-                    console.log("Fin");
-                    break;
-            }
-        }
-
-        let cantidadentradas = prompt("Antes de finalizar, ingresa cuántas entradas vas a comprar:");
-        const entrada = 3000;
-
-        let total = cantidadentradas * entrada + extrastotal;
-
-        alert(`El total a pagar por la cantidad de ${cantidadentradas} entradas y los agregados es de ${total} pesos. ¿Cómo deseas abonar?`);
-
-        let metodoPago;
-
-        do {
-            metodoPago = prompt(`Elige el metodo de pago a continuación:
-
-    1. Debito
-    2. Credito`);
-        } while (metodoPago !== "1" && metodoPago !== "2");
-
-        if (metodoPago === "2") {
-            let cuotas;
-
-            do {
-                cuotas = prompt(`Cantidad de cuotas disponibles el día de hoy:
-
-    1. 3 cuotas 
-    2. 6 cuotas`);
-            } while (cuotas !== "1" && cuotas !== "2");
-
-            switch (cuotas) {
-                case "1":
-                    alert(`Vas a pagar el total de ${total} pesos en 3 cuotas que serían de ${total / 3} pesos.`);
-                    break;
-                case "2":
-                    alert(`Vas a pagar el total de ${total} pesos en 6 cuotas que serían de  ${total / 6} pesos.`);
-                    break;
-                default:
-                    alert("La opción ingresada no está disponible.");
-                    break;
-            }
-        } else {
-            alert(`El precio total es de ${total} pesos.`);
-        }
-
-        alert(`¡Felicitaciones por llegar hasta aquí ${nombre}, esperamos tenerte de regreso pronto!`)
 }
+
+alert("Debemos verificar tu edad si quieres ver las películas en cartelera")
+let edad = prompt("Ingresa tu edad");
+
+if (edad >= 18) {
+    console.log("Ahora puedes continuar");
+} else {
+    console.log("Para ver estas películas debes ser mayor de edad");
+}
+
+let extras = "";
+let extrastotal = 0;
+
+while (extras !== "0" && extras !== "5") {
+    extras = prompt(`¡Elige que llevar!
+
+1. Gaseosa Grande (+$2500)
+2. Pochoclos Grande (+4000)
+3. Cerveza -lata- (+2000$)
+4. 2x1 Pochoclos Medianos y Dos Gaseosas (+7000)
+5. Continuar sin consumir
+0. Ingresa 0 para finalizar.
+`);
+
+    switch (extras) {
+        case "1":
+            extrastotal += 2500;
+            break;
+        case "2":
+            extrastotal += 4000;
+            break;
+        case "3":
+            extrastotal += 2000;
+            break;
+        case "4":
+            extrastotal += 7000;
+            break;
+        case "5":
+            break;
+        case "0":
+            break;
+        default:
+            console.log("Opción no válida");
+            break;
+    }
+}
+
+let cantidadentradas = prompt("Antes de finalizar, ingresa cuántas entradas vas a comprar:");
+const entrada = 3000;
+
+let total = cantidadentradas * entrada + extrastotal;
+
+alert(`El total a pagar por la cantidad de ${cantidadentradas} entradas y los agregados es de ${total} pesos. ¿Cómo deseas abonar?`);
+
+let metodoPago;
+
+do {
+    metodoPago = prompt(`Elige el metodo de pago a continuación:
+
+1. Debito
+2. Credito`);
+} while (metodoPago !== "1" && metodoPago !== "2");
+
+if (metodoPago === "2") {
+    let cuotas;
+
+    do {
+        cuotas = prompt(`Cantidad de cuotas disponibles el día de hoy:
+
+1. 3 cuotas 
+2. 6 cuotas`);
+    } while (cuotas !== "1" && cuotas !== "2");
+
+    switch (cuotas) {
+        case "1":
+            alert(`Vas a pagar el total de ${total} pesos en 3 cuotas que serían de ${total / 3} pesos.`);
+            break;
+        case "2":
+            alert(`Vas a pagar el total de ${total} pesos en 6 cuotas que serían de  ${total / 6} pesos.`);
+            break;
+        default:
+            alert("La opción ingresada no está disponible.");
+            break;
+    }
+} else {
+    alert(`El precio total es de ${total} pesos.`);
+}
+
+alert(`¡Felicitaciones por llegar hasta aquí ${nombre}, esperamos tenerte de regreso pronto!`);
